ee5f7d6032dbbdfc0cb77c49035efced33e22f55
Vita Filatova <filatova.vitta@gmail.com>
2022-11-05 16:54:14 +0200

    5/11/22

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 'vimeo - player',
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+const onPlay = function (data) {};
+
+player.on('timeupdate', onPlay);
+localStorage.setItem('videoplayer-current-time', onPlay);
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+populateTextarea();
+let formData = {};
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+console.log(formData);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateTextarea() {
+  const savedMassage = localStorage.getItem(STORAGE_KEY, JSON.parse(formData));
+  if (savedMassage) {
+    refs.form.value = savedMassage;
+  }
+}
